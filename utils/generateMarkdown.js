8a75fb2a8a8aed2e0102698c628fdb6dfef47171
BenBasic <benjaminmbasic@gmail.com>
2022-06-09 18:10:54 -0400

    Finished generateMarkdown function, also added renderLicenseTableContent function for creating a section in the table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,9 @@ function renderLicenseLink(badge) {
   if (badge === "None") {
     return ``;
   } else if (badge === "Creative Commons") {
-    return `https://creativecommons.org/terms#8`;
+    return `\nhttps://creativecommons.org/terms#8`;
   } else if (badge === "MIT") {
-    return `https://opensource.org/licenses/MIT`;
+    return `\nhttps://opensource.org/licenses/MIT`;
   } 
 };
 
@@ -24,10 +24,10 @@ function renderLicenseSection(badge) {
   if (badge === "None") {
     return ``;
   } else if (badge === "Creative Commons") {
-    return `## License
+    return `\n## License
     This work is licensed under Creative Commons Attribution 3.0 Unported License.`;
   } else if (badge === "MIT") {
-    return `## License
+    return `\n## License
     Copyright ${data.year} ${data.username}
 
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -39,6 +39,12 @@ function renderLicenseSection(badge) {
   }
 };
 
+function renderLicenseTableContent(badge) {
+  if (badge === "None") {
+    // Do nothing
+  } return `\n- [License](#license)`
+}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
@@ -46,11 +52,33 @@ function generateMarkdown(data) {
   # ${data.title}
   ${renderLicenseBadge(data.badge)}
 
-  ${data.badge}
-
   ## Description
 
   ${data.description}
+
+  ## Table Of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)${renderLicenseTableContent(data.badge)}
+  - [Contribution Guidelines](#Contribution Guidelines)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+  ${renderLicenseSection(data.badge)}${renderLicenseLink(data.badge)}
+  ## Contribution Guidelines
+
+  ${data.contribution}
+
+  ## Questions
+
+  This repo was created by https://github.com/${data.username}
+  To contact me, please send an email to ${data.email}
 `;
 };
 
